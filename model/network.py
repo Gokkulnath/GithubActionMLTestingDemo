675b07a3c51bfe3ca14d52dcf50563f7af165644
Gokkulnath T S <gokkulnath@gmail.com>
2024-11-17 18:51:58 +0530

    fix: make the parameters less than 25k

--- model/network.py
+++ model/network.py
@@ -6,16 +6,17 @@ class MNISTNet(nn.Module):
         super(MNISTNet, self).__init__()
         
         # First convolutional layer
-        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(8)
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
         
         # Second convolutional layer
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 16, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(16)
         
         # Fully connected layers
-        self.fc1 = nn.Linear(16 * 7 * 7, 32)
-        self.fc2 = nn.Linear(32, 10)
+        self.fc1 = nn.Linear(16 * 7 * 7, 10)
         
         self.pool = nn.MaxPool2d(2, 2)
         self.relu = nn.ReLU()
@@ -26,5 +27,4 @@ class MNISTNet(nn.Module):
         x = self.pool(self.relu(self.bn2(self.conv2(x))))
         x = x.view(-1, 16 * 7 * 7)
         x = self.dropout(self.relu(self.fc1(x)))
-        x = self.fc2(x)
         return x 
